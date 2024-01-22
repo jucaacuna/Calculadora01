e6dbf9e338d6971d5392a9fb97c6f4636b2600a1
Juan <juca.acuna@gmail.com>
2024-01-22 19:54:35 -0300

    Se adecúa el menú.

--- src/UI/Menu.java
+++ src/UI/Menu.java
@@ -3,6 +3,7 @@ package UI;
 import java.util.Scanner;
 
 import logica.Division;
+import logica.Matriz;
 import logica.Multiplicacion;
 import logica.OperacionA;
 import logica.Resta;
@@ -11,26 +12,63 @@ import logica.Suma;
 public class Menu {
 	Scanner s;
 	Byte a, b;
+	Matriz x, y, z;
 	
 	
 	Menu(){
 		
 	}
 	
-	String opciones () {
-		System.out.println(
-				"\n ######################################################### "
-				+ "\n Bienvenido a la calculadora."
-				+ "\n ######################################################### ");
-		System.out.print("Ingrese primer número: "); a = Byte.valueOf(s.nextByte()); //falta validar numero
-		System.out.print("Ingrese segundo número: "); b = Byte.valueOf(s.nextByte());
-		return operar();
+	String menu1() { 	//Crear menú genérico con parámetros: String opciones, bienvenida.
+		String op = "";
+		boolean bandera = true;
+		while(bandera) {
+			System.out.println(
+					"\n ######################################################### "
+					+ "\n Bienvenido a la calculadora."
+					+ "\n ######################################################### ");
+			System.out.println("Vamos a trabajar con: "
+					+ "\na - números"
+					+ "\nb - matrices"
+					+ "\ns - salir"
+					+ "\n -> ");
+			op = s.next().toString();
+			if (op.matches("[abs]")) {
+				bandera = false;
+			}
+		}
+		
+		return op;
 	}
 	
-	String operar() {
+	void operacionesNumeros() {
+		OperacionA operacion;
+		String op ="";
+		while (!op.contains("s")) {
+			op = menuNumeros();
+			if (op.contains("+")) {
+				operacion = new Suma(a, b);
+				System.out.println("La suma da: " + operacion.getResultado().toString());
+			} else if(op.contains("-")) {
+				operacion = new Resta(a, b);
+				System.out.println("La resta da: " + operacion.getResultado().toString());
+			} else if (op.contains("*")) {
+				operacion = new Multiplicacion(a, b);
+				System.out.println("La multiplicación da: " + operacion.getResultado().toString());
+			} else if(op.contains("/")) {
+				operacion = new Division(a, b);
+				System.out.println("La división da: " + operacion.getResultado().toString());
+			} else if(op.contains("s")) {
+				break;
+			}
+		}
+	}
+	
+	String menuNumeros() {
 		boolean bandera= true;
 		String op ="";
-		
+		System.out.print("Ingrese primer número: "); a = Byte.valueOf(s.nextByte()); //falta validar numero
+		System.out.print("Ingrese segundo número: "); b = Byte.valueOf(s.nextByte());
 		while(bandera) {
 			System.out.print("para sumar +\n"
 					+ "para restar -\n"
@@ -46,24 +84,20 @@ public class Menu {
 		return op;
 	}
 	
+	void operacionMatrices() {
+		System.out.println("MATRICES");
+	}
+	
 	void iniciar() {
 		s = new Scanner(System.in);
-		OperacionA operacion;
 		String op ="";
+		
 		while (!op.contains("s")) {
-			op = opciones();
-			if (op.contains("+")) {
-				operacion = new Suma(a, b);
-				System.out.println("La suma da: " + operacion.getResultado().toString());
-			} else if(op.contains("-")) {
-				operacion = new Resta(a, b);
-				System.out.println("La resta da: " + operacion.getResultado().toString());
-			} else if (op.contains("*")) {
-				operacion = new Multiplicacion(a, b);
-				System.out.println("La multiplicación da: " + operacion.getResultado().toString());
-			} else if(op.contains("/")) {
-				operacion = new Division(a, b);
-				System.out.println("La división da: " + operacion.getResultado().toString());
+			op = menu1();
+			if (op.contains("a")) {
+				operacionesNumeros();
+			} else if(op.contains("b")) {
+				operacionMatrices();
 			} else if(op.contains("s")) {
 				break;
 			}
